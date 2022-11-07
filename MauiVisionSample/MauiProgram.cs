996e681bf6ee9e7346ad739a978a9b340d207a5c
hschafer370 <90478800+hschafer370@users.noreply.github.com>
2022-11-06 22:21:32 -0500

    add files

---
+++ MauiVisionSample/MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using ZXing.Net.Maui;
+
+namespace MauiVisionSample;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseBarcodeReader()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		return builder.Build();
+	}
+}
